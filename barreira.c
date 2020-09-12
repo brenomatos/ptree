f4f72dfb2b278ae6e9baf1b7faba66cf22b4152e
Rennan Cordeiro <rennancl@gmail.com>
2020-09-12 15:54:21 -0300

    implements barrier

---
+++ barreira.c
@@ -0,0 +1,25 @@
+#include "barreira.h"
+
+void initBarreira(TBarreira* b, int n){
+    b->n = n;
+    b->counter = 0;
+    pthread_cond_t barreira_cond = PTHREAD_COND_INITIALIZER;
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+}
+
+void barreira(TBarreira* b){
+    // trava o mutex
+    pthread_mutex_lock(&(b->mutex));
+    b->counter++;
+    if (b->counter <= b->n) {
+        // enquanto o número de threads
+        // na barreira nao chega ao máximo espera
+        pthread_cond_wait(&(b->barreira_cond), &(b->mutex));
+    } else {
+        // quando todos estão nela, libera
+        b->counter == 0;
+        pthread_cond_signal(&(b->barreira_cond));
+    }
+    // destrava o mutex
+    pthread_mutex_unlock(&(b->mutex));
+}
--- barreira.h
+++ barreira.h
@@ -1,4 +1,15 @@
-TBarreira bar;
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+struct TBarreira {
+    int n;
+    int counter;
+    pthread_cond_t barreira_cond;
+    pthread_mutex_t mutex;
+};
+
+typedef struct TBarreira TBarreira;
 
 void initBarreira(TBarreira* b, int n);
-void barreira(&bar);
+void barreira(TBarreira* b);
