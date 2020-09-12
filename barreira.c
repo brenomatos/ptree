5678291c8e8d1cd48be6cf87709dfe7a6a864081
Rennan Cordeiro <rennancl@gmail.com>
2020-09-12 16:02:02 -0300

    casts pthread constants

--- barreira.c
+++ barreira.c
@@ -3,8 +3,8 @@
 void initBarreira(TBarreira* b, int n){
     b->n = n;
     b->counter = 0;
-    b->barreira_cond = PTHREAD_COND_INITIALIZER;
-    b->mutex = PTHREAD_MUTEX_INITIALIZER;
+    b->barreira_cond = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
+    b->mutex = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
 }
 
 void barreira(TBarreira* b){
